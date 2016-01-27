edb6e804c0fe55dd6594f8f52cf1feb5f86857ce
Konstantin Hantsov <hantsov@cantemo.com>
2016-01-27 10:57:54 +0100

    Python 2.6 support

--- setup.py
+++ setup.py
@@ -3,17 +3,17 @@ from setuptools import setup
 version = '0.2.4'
 
 setup(
-  name = 'django-webpack-loader',
-  packages = ['webpack_loader', 'webpack_loader/templatetags', 'webpack_loader/contrib'],
-  version = version,
-  description = 'Transparently use webpack with django',
-  author = 'Owais Lone',
-  author_email = 'hello@owaislone.org',
-  download_url = 'https://github.com/owais/django-webpack-loader/tarball/{}'.format(version),
-  url = 'https://github.com/owais/django-webpack-loader', # use the URL to the github repo
-  keywords = ['django', 'webpack', 'assets'], # arbitrary keywords
-  data_files = [("", ["LICENSE"])],
-  classifiers = [
+  name='django-webpack-loader',
+  packages=['webpack_loader', 'webpack_loader/templatetags', 'webpack_loader/contrib'],
+  version=version,
+  description='Transparently use webpack with django',
+  author='Owais Lone',
+  author_email='hello@owaislone.org',
+  download_url='https://github.com/owais/django-webpack-loader/tarball/{0}'.format(version),
+  url='https://github.com/owais/django-webpack-loader', # use the URL to the github repo
+  keywords=['django', 'webpack', 'assets'],  # arbitrary keywords
+  data_files=[("", ["LICENSE"])],
+  classifiers=[
     'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3.3',
     'Programming Language :: Python :: 3.4',
